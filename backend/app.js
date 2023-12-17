7beb329b6862c57054addfc05189faf20f1d9b4d
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-12-18 00:21:26 +0530

    fixed errors

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require('cors');
-const app = express()
+const cors = require("cors");
+const app = express();
 app.use(cors());
 const connectDB = require("./database/connect");
 const PORT = process.env.PORT || 8000;
@@ -10,21 +10,21 @@ const colleges_routes = require("./routes/colleges");
 const states_routes = require("./routes/states");
 
 app.get("/", (req, res) => {
-    res.send("Hi, I am a college API");
-})
+  res.send("Hi, I am a college API");
+});
 
 //middleware or setting router
 app.use("/api/colleges", colleges_routes);
 app.use("/api/states", states_routes);
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGODB_URL);
-        app.listen(PORT, () => {
-            console.log(`${PORT} Connected`);
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await connectDB(process.env.MONGODB_URL);
+    app.listen(PORT, () => {
+      console.log(`${PORT} Connected`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 start();
