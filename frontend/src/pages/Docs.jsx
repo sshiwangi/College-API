272b4fc6583dbb63d9641847dbabd2bc19099702
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-12-19 19:08:53 +0530

    updating docs styles

--- frontend/src/pages/Docs.jsx
+++ frontend/src/pages/Docs.jsx
@@ -19,25 +19,29 @@ function Docs() {
      <div className=" z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidenav">
       <div className="overflow-y-auto py-5 px-5 h-full bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
         <div className="space-y-2">
-          <span onClick={() => handleItemClick('gettingStarted')}>Getting Started</span>
+          <span onClick={() => handleItemClick('gettingStarted')} className={`cursor-pointer ${selectedItem === 'gettingStarted' ? 'text-white bg-gray-600 font-bold' : 'text-black hover:text-gray-700 focus:text-gray-700 focus:outline-none'} rounded-md px-3 py-2`}> Getting Started</span>
         </div>
         <div className="pt-5 space-y-2 " >
-          <div onClick={() => setShowAPIOptions(!showAPIOptions)}>API</div>
+          <div onClick={() => {
+  setShowAPIOptions(!showAPIOptions);
+  handleItemClick('api');
+}} className={`cursor-pointer ${selectedItem === 'api' ? 'text-white bg-gray-600 font-bold' : 'text-black hover:text-gray-700 focus:text-gray-700 focus:outline-none'} rounded-md px-3 py-2`}>API</div>
           {showAPIOptions && (
               <ul>
-                <li onClick={() => handleItemClick('allStates')}>All States</li>
-                <li onClick={() => handleItemClick('allColleges')}>All Colleges</li>
-                <li onClick={() => handleItemClick('collegesByStates')}>Colleges By States</li>
+                <li onClick={() => handleItemClick('allStates')} className={`cursor-pointer ${selectedItem === 'allStates' ? 'text-white bg-gray-600 font-bold' : 'text-black hover:text-gray-700 focus:text-gray-700 focus:outline-none'} rounded-md px-3 py-2 my-3`}>All States</li>
+                <li onClick={() => handleItemClick('allColleges')} className={`cursor-pointer ${selectedItem === 'allColleges' ? 'text-white bg-gray-600 font-bold' : 'text-black hover:text-gray-700 focus:text-gray-700 focus:outline-none'} rounded-md px-3 py-2 my-3`}>All Colleges</li>
+                <li onClick={() => handleItemClick('collegesByStates')} className={`cursor-pointer ${selectedItem === 'collegesByStates' ? 'text-white bg-gray-600 font-bold' : 'text-black hover:text-gray-700 focus:text-gray-700 focus:outline-none'} rounded-md px-3 py-2 my-3`}>Colleges By States</li>
               </ul>
             )}
         </div>
-        <div className="pt-5 space-y-2" onClick={() => handleItemClick('contributing')}>
+        <div onClick={() => handleItemClick('contributing')} className={`cursor-pointer ${selectedItem === 'contributing' ? 'text-white bg-gray-600 font-bold' : 'text-black hover:text-gray-700 focus:text-gray-700 focus:outline-none'} rounded-md px-3 py-2 pt-5 space-y-2`}>
           Contributing
         </div>
       </div>
     </div>
     <div className='h-screen text-gray-900 w-full px-4 py-16'>
     {selectedItem === 'gettingStarted' && <GettingStarted />}
+    {selectedItem === 'api' && <GettingStarted />}
         {selectedItem === 'allStates' && <AllStates />}
         {selectedItem === 'allColleges' && <AllColleges />}
         {selectedItem === 'collegesByStates' && <CollegesByStates />}
